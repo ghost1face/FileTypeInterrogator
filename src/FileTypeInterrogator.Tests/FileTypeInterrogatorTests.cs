b2b679230ddfc10526de2145c970a4adb3825b83
Daniel Destouche <daniel.destouche@gmail.com>
2019-07-06 22:51:06 -0400

    Updated solution file & tests v0.0.1

---
+++ src/FileTypeInterrogator.Tests/FileTypeInterrogatorTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileTypeInterrogator.Tests
+{
+    [TestClass]
+    public class FileTypeInterrogatorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
