3c283f67028bda8c056fcc676865047c16e3ee0f
Daniel Destouche <daniel.destouche@gmail.com>
2023-01-10 20:05:19 -0500

    Cleanup documentation

--- src/FileTypeInterrogator/FileTypeInfo.cs
+++ src/FileTypeInterrogator/FileTypeInfo.cs
@@ -7,7 +7,8 @@ namespace FileTypeInterrogator
     /// </summary>
     public class FileTypeInfo
     {
-        internal FileTypeInfo(string name, string fileType, string mimeType, byte[] header, string[] alias = null, int offset = 0, byte[] subHeader = null)
+        internal FileTypeInfo(string name, string fileType, string mimeType, byte[] header, 
+            string[] alias = null, int offset = 0, byte[] subHeader = null)
         {
             Name = name;
             MimeType = mimeType;
@@ -21,9 +22,6 @@ namespace FileTypeInterrogator
         /// <summary>
         /// Gets the name of the file type.
         /// </summary>
-        /// <value>
-        /// The name.
-        /// </value>
         public string Name { get; private set; }
 
         /// <summary>
@@ -44,25 +42,16 @@ namespace FileTypeInterrogator
         /// <summary>
         /// Gets unique header 'Magic Numbers' to identifiy this file type
         /// </summary>
-        /// <value>
-        /// The header.
-        /// </value>
         public byte[] Header { get; private set; }
 
         /// <summary>
         /// Gets the offset location of the Header details
         /// </summary>
-        /// <value>
-        /// The offset.
-        /// </value>
         public int Offset { get; private set; }
 
         /// <summary>
         /// Gets the additional identifier to guarantee uniqueness of the file type
         /// </summary>
-        /// <value>
-        /// The additional identifier.
-        /// </value>
         public byte[] SubHeader { get; private set; }
 
         /// <inheritdoc/>
